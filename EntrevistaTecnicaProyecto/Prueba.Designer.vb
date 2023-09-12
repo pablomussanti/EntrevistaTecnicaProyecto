4788daae521d8d7e4095315346dd301f5ff3fd23
PabloM <pablomussanti@hotmail.com>
2023-09-12 10:53:26 -0300

    ABM Test con Region

--- EntrevistaTecnicaProyecto/Prueba.Designer.vb
+++ EntrevistaTecnicaProyecto/Prueba.Designer.vb
@@ -25,6 +25,11 @@ Partial Class Prueba
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -38,25 +43,74 @@ Partial Class Prueba
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(612, 135)
+        Me.Button1.Location = New System.Drawing.Point(654, 108)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(100, 23)
         Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "Crear/Modificar"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(612, 109)
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Location = New System.Drawing.Point(654, 39)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(100, 20)
         Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(654, 65)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(654, 137)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(100, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Eliminar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(608, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Codigo"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(585, 68)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(63, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Descripcion"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(654, 166)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 23)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "Limpiar"
+        Me.Button3.UseVisualStyleBackColor = True
         '
         'Prueba
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 231)
+        Me.ClientSize = New System.Drawing.Size(800, 215)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.DataGridView1)
@@ -71,4 +125,9 @@ Partial Class Prueba
     Friend WithEvents DataGridView1 As DataGridView
     Friend WithEvents Button1 As Button
     Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button3 As Button
 End Class
--- EntrevistaTecnicaProyecto/Prueba.vb
+++ EntrevistaTecnicaProyecto/Prueba.vb
@@ -5,10 +5,73 @@ Public Class Prueba
     Private Sub Enlazar()
         DataGridView1.DataSource = Nothing
         DataGridView1.DataSource = region.ListarRegion()
-        'Me.DataGridView1.Columns(0).Visible = False
+        'Me.DataGridView1.Columns(1).Visible = False
     End Sub
 
     Private Sub Prueba_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Enlazar()
+        Try
+
+            Enlazar()
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+
+    Private Sub DataGridView1_SelectionChanged(sender As Object, e As EventArgs) Handles DataGridView1.SelectionChanged
+
+        Dim l As DataGridViewSelectedRowCollection = Me.DataGridView1.SelectedRows
+
+        If l.Count = 1 Then
+            TextBox2.Text = DataGridView1.CurrentRow.Cells.Item(0).Value.ToString
+            TextBox1.Text = DataGridView1.CurrentRow.Cells.Item(1).Value.ToString
+        End If
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim oRegion As New Region
+
+            If TextBox1.Text = "" Then
+                oRegion.RegionDescription = TextBox2.Text
+                region.CrearRegion(oRegion)
+            Else
+                oRegion.Id = TextBox1.Text
+                oRegion.RegionDescription = TextBox2.Text
+                region.ModificarRegion(oRegion)
+            End If
+
+            Enlazar()
+
+        Catch ex As Exception
+            MessageBox.Show(String.Format("Creacion/Modificacion Fallida, {0}", ex.Message))
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+
+            Dim oRegion As New Region
+
+            If TextBox1.Text = "" Then
+                Throw New Exception()
+            Else
+                oRegion.Id = TextBox1.Text
+                region.EliminarRegion(oRegion)
+            End If
+
+            Enlazar()
+
+        Catch ex As Exception
+            MessageBox.Show(String.Format("Eliminacion Fallida, {0}", ex.Message))
+        End Try
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
     End Sub
 End Class
